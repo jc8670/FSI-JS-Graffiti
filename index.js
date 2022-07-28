52b0323ab54326463c94180493a4c36d73d26711
jc8670 <jc8670@gmail.com>
2022-07-27 19:54:00 -0700

    added whitespace

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 let header = document.querySelector('#page-header');
 
 let dogImages = document.querySelectorAll('.dog-image')
+
 for(let i = 0; i < dogImages.length; i++){
 	dogImages[i].style.borderRadius = '50%'
 }
