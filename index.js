048123bb9ec980a0a9c70fe3f2e3bf0251e737fe
jc8670 <jc8670@gmail.com>
2022-07-27 19:48:54 -0700

    changed heading alignment to left radius to photos

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header');
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50%'
+}
+
+
+header.style.textAlign = "left";
